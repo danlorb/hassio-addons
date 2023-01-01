45d370b0f713f0a8b70a494f3dfbea93f36fe769
Roland Breitschaft <breitschaftroland@gmail.com>
2023-01-02 00:30:30 +0100

    Fix Logging Errors

--- matrix/rootfs/etc/cont-init.d/matrix.sh
+++ matrix/rootfs/etc/cont-init.d/matrix.sh
@@ -4,7 +4,8 @@
 # This file init Matrix
 # ==============================================================================
 
-bashio::log.level "$(bashio::config 'log_level' 'warning')"
+loglevel=$(bashio::config 'log_level' 'warning')
+bashio::log.level "$loglevel"
 
 config="/config/addons_config/matrix"
 servername=$(bashio::config 'server_name' 'example.org')
@@ -24,4 +25,43 @@ if [ ! -f "$config"/homeserver.yaml ]; then
         --data-directory /share/matrix \
         --report-stats="$reportstats" \
         --open-private-ports
+
+    if [ "$loglevel" == "trace" ]; then
+        loglevel="debug"
+    fi
+
+    if [ "$loglevel" == "notice" ]; then
+        loglevel="info"
+    fi
+
+    if [ "$loglevel" == "fatal" ]; then
+        loglevel="critical"
+    fi
+
+    loglevelUpper=$(bashio::string.upper "$loglevel")
+    cat > "$config"/"$servername".log.config <<EOF
+version: 1
+
+formatters:
+    precise:
+        format: '%(asctime)s - %(name)s - %(lineno)d - %(levelname)s - %(request)s - %(message)s'
+
+handlers:
+    console:
+        class: logging.StreamHandler
+        formatter: precise
+
+loggers:
+    synapse.storage.SQL:
+        # beware: increasing this to DEBUG will make synapse log sensitive
+        # information such as access tokens.
+        level: loglevelUpper
+
+root:
+    level: $loglevelUpper
+    handlers: [console]
+
+disable_existing_loggers: true
+
+EOF
 fi
