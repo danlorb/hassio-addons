5bc7ac2d7a991e7f78eb45a62efdd659a864bd21
Roland Breitschaft <breitschaftroland@gmail.com>
2023-01-01 23:41:54 +0100

    Add fixes when Services starting

--- matrix/rootfs/etc/cont-init.d/matrix.sh
+++ matrix/rootfs/etc/cont-init.d/matrix.sh
@@ -7,20 +7,21 @@
 bashio::log.level "$(bashio::config 'log_level' 'warning')"
 
 config="/config/addons_config/matrix"
-servername=$(bashio::config 'server_name')
+servername=$(bashio::config 'server_name' 'example.org')
 reportstats="no"
 if bashio::config.true 'report_stats'; then
     reportstats="yes"
 fi
 
 if [ ! -f "$config"/homeserver.yaml ]; then
+    bashio::log.info "Create Matrix Service Configuration..."
     mkdir -p "${config}"
 
     /opt/venvs/matrix-synapse/bin/synapse_homeserver \
         --generate-config \
         -H "$servername" \
         -c "$config"/homeserver.yaml \
-        --data-directory /media/matrix \
+        --data-directory /share/matrix \
         --report-stats="$reportstats" \
         --open-private-ports
 fi
