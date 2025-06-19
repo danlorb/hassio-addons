10f75573b4c74426daa62deffb720990264f5574
Roland Breitschaft <gitea@danlor.simplelogin.com>
2025-06-19 09:17:53 +0200

    deprecate guacamole

--- guacamole/.devcontainer/post-create.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# -*- coding: utf-8 -*-
-
-apt-get update && apt-get install -qy --no-install-recommends procps
-mkdir -p /tmp/.bashio
-ln -s /data/options.json /tmp/.bashio/addons.self.options.config.cache
--- guacamole/CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-
-## 1.0.0
-
-- First Release
-
-## 1.1.0
-
-- Change Storage Location for Recordings
